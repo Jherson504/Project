f8aa965bb9557cd6cf571421f2b60990b0090f7e
Jherson504 <jhersonclorado08@gmail.com>
2022-04-03 17:09:35 +0800

    .

--- base/models.py
+++ base/models.py
@@ -9,7 +9,6 @@ class Tag(models.Model):
     def __str__(self) -> str:
         return str(self.name)
 
-    
     class Meta:
         ordering = ['-created', 'name']
 
@@ -21,59 +20,67 @@ class Topic(models.Model):
     def __str__(self) -> str:
         return str(self.name)
 
-    
     class Meta:
         ordering = ['-created', 'name']
 
+
 class Notebook(models.Model):
     tags = models.ManyToManyField(Tag, blank=True,  related_name='notebooks')
-    topics = models.ManyToManyField(Topic, blank=True,  related_name='notebooks')
+    topics = models.ManyToManyField(
+        Topic, blank=True,  related_name='notebooks')
     name = models.CharField(max_length=200)
+    description = models.TextField(default="No Description")
     owner = models.ForeignKey(User, null=True, on_delete=models.SET_NULL)
     created = models.DateTimeField(auto_now_add=True)
 
     def __str__(self) -> str:
         return str(self.name)
 
-    
     class Meta:
         ordering = ['-created', 'name']
 
+
 class Lecture(models.Model):
-    notebook = models.ForeignKey(Notebook, null=True, on_delete=models.SET_NULL)
+    notebook = models.ForeignKey(
+        Notebook, null=True, on_delete=models.SET_NULL)
     tags = models.ManyToManyField(Tag, blank=True,  related_name='lectures')
-    topics = models.ManyToManyField(Topic, blank=True,  related_name='lectures')
+    topics = models.ManyToManyField(
+        Topic, blank=True,  related_name='lectures')
     title = models.CharField(max_length=200)
     owner = models.ForeignKey(User, null=True, on_delete=models.SET_NULL)
     created = models.DateTimeField(auto_now_add=True)
+
     def __str__(self) -> str:
         return str(self.title)
 
-    
     class Meta:
         ordering = ['-created', 'title']
 
+
 class Page(models.Model):
+    notebook = models.ForeignKey(
+        Notebook, null=True, related_name='pages', on_delete=models.CASCADE)
     index = models.PositiveSmallIntegerField()
 
     def __str__(self) -> str:
         return str(super(models.Model, self).sections.title)
 
+
 class Section(models.Model):
-    page = models.ForeignKey(Page, related_name='sections', on_delete=models.CASCADE)
+    page = models.ForeignKey(
+        Page, related_name='sections', blank=True, null=True, on_delete=models.CASCADE)
     lecture = models.ForeignKey(Lecture, null=True, on_delete=models.SET_NULL)
     tags = models.ManyToManyField(Tag, blank=True,  related_name='sections')
-    topics = models.ManyToManyField(Topic, blank=True,  related_name='sections')
+    topics = models.ManyToManyField(
+        Topic, blank=True,  related_name='sections')
     title = models.CharField(max_length=200)
 
     body = models.TextField(null=True, blank=True)
 
     created = models.DateTimeField(auto_now_add=True)
 
-
     def __str__(self) -> str:
         return str(self.title)
 
-    
     class Meta:
         ordering = ['-created', 'title']
--- base/views.py
+++ base/views.py
@@ -1,6 +1,18 @@
 from django.shortcuts import render
-
+from .models import Lecture, Notebook, Page, Section, Tag, Topic
+from django.db.models.query_utils import Q
 
 
 def home(request):
-    return render(request, 'base/homepage/main.html')
+    query = ''
+    # request_form = request.dict()
+
+    all_notebooks = Notebook.objects.all()
+    all_lectures = Lecture.objects.all()
+
+    notebooks = all_notebooks.filter(
+        Q(tags__name__icontains=query)
+    ).distinct()
+
+    context = {"notebooks": notebooks, "all_lectures": all_lectures}
+    return render(request, 'base/homepage/main.html', context)
