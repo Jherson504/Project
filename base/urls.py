78b96f5fd83dd312d2efa60465e7b5e68486899e
Jherson504 <jhersonclorado08@gmail.com>
2022-04-05 21:37:10 +0800

    .

--- base/urls.py
+++ base/urls.py
@@ -4,4 +4,10 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+
+    path('auth/', views.authenticate_user, name='authenticate-user'),
+    path('auth/login/', views.login_user, name='login-user'),
+    path('auth/logout/', views.logout_user, name='logout-user'),
+    path('auth/sign-up/', views.sign_up_user, name='sign-up-user'),
+
 ]
--- base/views.py
+++ base/views.py
@@ -1,6 +1,52 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from django.contrib.auth import authenticate, logout, login
 from .models import Lecture, Notebook, Page, Section, Tag, Topic
 from django.db.models.query_utils import Q
+from django.contrib import messages
+
+
+def authenticate_user(request):
+    if request.user.is_authenticated:
+        return redirect('logout-user')
+    return redirect('login-user')
+
+
+def login_user(request):
+    if request.user.is_authenticated:
+        return redirect('home-page')
+
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        user = authenticate(username=username, password=password)
+        print(username, password, user)
+        if user:
+            login(request, user)
+            return redirect('home')
+        else:
+            messages.error(request, "Invalid username or password")
+            return render(request, 'base/auth/login.html')
+
+    return render(request, 'base/auth/login.html')
+
+
+def logout_user(request):
+    if not request.user.is_authenticated:
+        return redirect('home-page')
+
+    if request.method == 'GET':
+        if request.GET.get('confirm'):
+            logout(request)
+            return redirect('home')
+
+    return render(request, 'base/auth/logout.html')
+
+
+def sign_up_user(request):
+    if request.user.is_authenticated:
+        return redirect('home-page')
+    return render(request, 'base/auth/sign-up.html')
 
 
 def home(request):
